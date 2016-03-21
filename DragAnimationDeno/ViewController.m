4fb5a6c1023701967b369f36b250dfae00300e4d
8kana <8kana@8kanadeimac.local>
2016-03-21 09:59:12 +0800

    Initial Commit

---
+++ DragAnimationDeno/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DragAnimationDeno
+//
+//  Created by 8kana on 16/3/21.
+//  Copyright © 2016年 8kana. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ DragAnimationDenoTests/DragAnimationDenoTests.m
@@ -0,0 +1,39 @@
+//
+//  DragAnimationDenoTests.m
+//  DragAnimationDenoTests
+//
+//  Created by 8kana on 16/3/21.
+//  Copyright © 2016年 8kana. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DragAnimationDenoTests : XCTestCase
+
+@end
+
+@implementation DragAnimationDenoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
